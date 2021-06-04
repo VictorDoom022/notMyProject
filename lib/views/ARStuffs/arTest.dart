ddc11face02be943401dfe7f532578862ae6e016
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-05 01:48:14 +0800

    add images to AR

--- lib/views/ARStuffs/arTest.dart
+++ lib/views/ARStuffs/arTest.dart
@@ -30,21 +30,126 @@ class _arTestState extends State<arTest> {
     arCoreController = controller;
 
     _addImage(arCoreController);
+    _addSecondImage(arCoreController);
+    _addThirdImage(arCoreController);
+    _addFourthImage(arCoreController);
+    _addFifthImage(arCoreController);
+    _addSixthImage(arCoreController);
+    _addSeventhImage(arCoreController);
+    _addEigthImage(arCoreController);
     // _addSphere(arCoreController);
     // _addCylindre(arCoreController);
     // _addCube(arCoreController);
   }
 
   void _addImage(ArCoreController controller) async {
-    Uint8List data = (await rootBundle.load('assets/images/aHuman.png')).buffer.asUint8List();
+    Uint8List data = (await rootBundle.load('assets/images/kukup/bridge new.jpg')).buffer.asUint8List();
     final image = ArCoreImage(
         bytes: data,
-        width: 300,
-        height: 300
+        width: 900,
+        height: 2200
     );
     final node = ArCoreNode(
       image: image,
-      position: vector.Vector3(0, 0, -1.5),
+      position: vector.Vector3(0, -2, -3.5),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addSecondImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/kukup tree.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(1.5, -2, -3.3),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addThirdImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/kukup scene.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(3.0, -2, -3.3),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addFourthImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/kukup scene 1.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(4.5, -2, -3.3),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addFifthImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/kukup scene 2.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(-1.5, -2, -3.3),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addSixthImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/kukup taman.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(-3.0, -2, -3.3),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addSeventhImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/kukup view.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(0, 1.5, -3.3),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addEigthImage(ArCoreController controller) async {
+    Uint8List data = (await rootBundle.load('assets/images/kukup/monkey.jpg')).buffer.asUint8List();
+    final image = ArCoreImage(
+        bytes: data,
+        width: 900,
+        height: 2200
+    );
+    final node = ArCoreNode(
+      image: image,
+      position: vector.Vector3(1.5, 1.5, -3.3),
     );
     controller.addArCoreNode(node);
   }
