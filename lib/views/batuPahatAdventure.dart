ea4f07d690b7fdf69345fb27a3f52ed670142d3b
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-05 22:58:14 +0800

    add gunung banang

--- lib/views/batuPahatAdventure.dart
+++ lib/views/batuPahatAdventure.dart
@@ -3,6 +3,8 @@ import 'package:notmyproject/views/batuPahatInfo.dart';
 import 'package:notmyproject/views/kukupIslandInfoPage.dart';
 import 'package:notmyproject/views/pontianInfo.dart';
 
+import 'ARStuffs/locationAR.dart';
+
 class batuPahatAdventurePage extends StatefulWidget {
   @override
   _batuPahatAdventurePageState createState() => _batuPahatAdventurePageState();
@@ -72,9 +74,20 @@ class _batuPahatAdventurePageState extends State<batuPahatAdventurePage> {
                       onTap: (){
                         Navigator.push(context, MaterialPageRoute(
                             builder:(context){
-                              return kukupIslandInfoPage();
+                              return locationAR(
+                                appBatTitle: 'BATU PAHAT > ADVENTURE > GUNUNG BANANG',
+                                imageDescImageSrc: 'assets/images/gunungBanang/gunung banang loc.JPG',
+                                firstImageSrc: 'assets/images/gunungBanang/gunungbanang1.jpg',
+                                secImageSrc: 'assets/images/gunungBanang/gunungbanang2.jpg',
+                                thirdtImageSrc: 'assets/images/gunungBanang/gunung banang 3.jpg',
+                                fourthImageSrc: 'assets/images/gunungBanang/gunung banang 4.jpg',
+                                fifthImageSrc: 'assets/images/gunungBanang/gunung banang5.jpg',
+                                sixthImageSrc: 'assets/images/gunungBanang/gunungbanang6.jpg',
+                                seventhImageSrc: 'assets/images/gunungBanang/gunung banang7.jpg',
+                                eightImageSrc: 'assets/images/gunungBanang/gunung banang 8.JPG',
+                              );
                             }
-                          )
+                        )
                         );
                       },
                       child: moreDetailsButton('assets/images/moreDetails.png', 'More details')
